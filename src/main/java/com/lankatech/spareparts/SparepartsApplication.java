3ada68d202458aba4e3008ba244c5234141b8272
IT25102546 <it25102546@my.sliit.lk>
2026-09-21 16:50:45 +0530

    Open system homepage automatically on startup

--- src/main/java/com/lankatech/spareparts/SparepartsApplication.java
+++ src/main/java/com/lankatech/spareparts/SparepartsApplication.java
@@ -2,6 +2,8 @@ package com.lankatech.spareparts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SparepartsApplication {
@@ -10,4 +12,21 @@ public class SparepartsApplication {
 		SpringApplication.run(SparepartsApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void openBrowser() {
+		try {
+			new ProcessBuilder(
+					"cmd",
+					"/c",
+					"start",
+					"",
+					"http://localhost:8080/"
+			).start();
+
+			System.out.println("Browser opened successfully.");
+		} catch (Exception e) {
+			System.out.println("Browser could not be opened automatically.");
+			e.printStackTrace();
+		}
+	}
 }
