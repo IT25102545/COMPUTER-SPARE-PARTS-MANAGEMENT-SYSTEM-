07ea8c9e13ad9e194b947ad1953c6b2970e53e4c
IT25102546 <it25102546@my.sliit.lk>
2026-09-22 12:32:35 +0530

    Implement authentication and protected system access

--- src/main/java/com/lankatech/spareparts/auth/repository/UserRepository.java
+++ src/main/java/com/lankatech/spareparts/auth/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.lankatech.spareparts.auth.repository;
 import com.lankatech.spareparts.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
 }
